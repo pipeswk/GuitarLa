08da594c547a29ad4332f86d58576b5ffa690d6b
pipeswk <afco1995@gmail.com>
2022-03-31 17:29:04 -0500

    Pagina Blog v1

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import Layout from '../Components/Layout'
+
+const Blog = () => {
+  return (
+    <Layout
+        pagina='Blog'
+    >
+        <h1>Blog</h1>
+    </Layout>
+  )
+}
+
+export default Blog
