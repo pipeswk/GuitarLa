4a610c8814ce62091c4ab758e287a2d0b7a76c7f
pipeswk <afco1995@gmail.com>
2022-03-31 17:30:02 -0500

    Pagina Tienda v1

---
+++ pages/tienda.js
@@ -0,0 +1,13 @@
+import Layout from '../Components/Layout'
+
+const Tienda = () => {
+  return (
+    <Layout
+        pagina='Tienda'
+    >
+        <h1>Tienda Virtual</h1>
+    </Layout>
+  )
+}
+
+export default Tienda
