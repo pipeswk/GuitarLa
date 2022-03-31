fbfe19df3d6a51c0cc3c7c4990f2255cba3b1c93
pipeswk <afco1995@gmail.com>
2022-03-31 13:17:17 -0500

    Componente nosotros v1

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Layout from '../Components/Layout'
+
+const Nosotros = () => {
+  return (
+    <Layout
+        pagina='Nosotros'
+    >
+        <h1>Desde nosotros</h1>
+    </Layout>
+  )
+}
+
+export default Nosotros
