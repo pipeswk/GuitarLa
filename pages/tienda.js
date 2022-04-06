c1abf139fcfd695076042f39f2db987bb130bb98
pipeswk <afco1995@gmail.com>
2022-04-05 23:01:16 -0500

    Pagina tienda v1

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,13 +1,33 @@
 import Layout from '../Components/Layout'
+import Listado from '../Components/Listado'
 
-const Tienda = () => {
+const Tienda = ( { productos } ) => {
+
+
   return (
     <Layout
         pagina='Tienda'
     >
-        <h1>Tienda Virtual</h1>
+        <main className='contenedor'>
+          <h1 className='heading'>Nuestra colección</h1>
+          <Listado
+            productos={productos}
+          />
+        </main>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras`
+  const respuesta = await fetch(url)
+  const productos = await respuesta.json()
+  console.log('Productos leidos')
+  return {
+    props: {
+      productos
+    }
+  }
 }
 
 export default Tienda
