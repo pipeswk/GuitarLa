0bf0f44b92ffb3d32a8c72e1b8c95240f67aa246
pipeswk <afco1995@gmail.com>
2022-04-05 23:01:04 -0500

    Pagina Blog v1

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,44 @@
+import { useEffect } from 'react'
 import Layout from '../Components/Layout'
+import Entrada from '../Components/Entrada'
+import styles from '../styles/Blog.module.css'
 
-const Blog = () => {
+
+const Blog = ( { resultado } ) => {  
+  
   return (
     <Layout
         pagina='Blog'
     >
-        <h1>Blog</h1>
+        <main className='contenedor'>
+          <h2 className='heading'>Blog</h2>
+
+          <div className={styles.blog}>
+            {resultado.map( (entrada) => (
+              <Entrada 
+                key={entrada.id}
+                entrada={entrada}
+              />
+            ) )}
+          </div>
+
+        </main>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+
+  const url = `${process.env.API_URL}/blogs`;
+  const respuesta = await fetch(url);
+  const resultado = await respuesta.json();
+  // Si se aplica un consolo.log(resultado) no se visualiza en la consola cliente sino en la consola server
+  return {
+    props: {
+      resultado
+    }
+  }
+
 }
 
 export default Blog
