9d23976a91f5fa43f1cd92b2d636064f940910aa
pipeswk <afco1995@gmail.com>
2022-04-08 16:03:18 -0500

    Se importa producto y se pasa al header

--- Components/Layout.js
+++ Components/Layout.js
@@ -2,18 +2,22 @@ import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = ( { children, pagina } ) => {
+const Layout = ( { children, pagina, producto } ) => {
   return (
     <div>
         <Head>
             <title>{`GuitarLa - ${pagina}`}</title>
             <meta name='desription' content='Sitio web de venta de guitarras' />
         </Head>
-        <Header />
+        <Header producto={producto} />
         {children}
         <Footer />
     </div>
   )
+}
+
+Layout.defaultProps = {
+  producto: null
 }
 
 export default Layout
