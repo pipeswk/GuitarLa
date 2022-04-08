ec0ead3316167b2bdd074188190a84afa1cae8d7
pipeswk <afco1995@gmail.com>
2022-04-08 16:02:24 -0500

    Se agrega useRouer, carrito y guitarra de promo

--- Components/Header.js
+++ Components/Header.js
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import { useRouter } from 'next/router'
 
-const Header = () => {
+const Header = ( { producto } ) => {
+
+    const router = useRouter()
+    
+    const valor = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(producto?.precio)
+
   return (
     <header className={styles.header}>
         <div className='contenedor'>
@@ -19,10 +25,33 @@ const Header = () => {
                     <Link href='/nosotros'>Nosotros</Link>
                     <Link href='/blog'>Blog</Link>
                     <Link href='/tienda'>Tienda</Link>
+                    <Link href='/carrito'>
+                        <a>
+                            <Image layout='fixed' width={30} height={25} alt='carrito' src='/img/carrito.png' />
+                        </a>
+                    </Link>
                 </nav>
             </div>
 
+            {producto && (
+                <div className={styles.modelo}>
+                    <h2>{producto.nombre}</h2>
+                    <p className={styles.descripcion}>{producto.descripcion}</p>
+                    <p className={styles.precio}>{valor}</p>
+                    <Link href={`/guitarras/${producto.url}`}>
+                        <a className={styles.boton}>
+                            Ver producto
+                        </a>
+                    </Link>
+                </div>
+            )}
+
         </div>
+
+        {router.route === '/' && (
+            <img className={styles.guitarra} src='/img/header_guitarra.png' alt='Header guitarra' />
+        )}
+
     </header>
   )
 }
